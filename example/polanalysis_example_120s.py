b41e705af4349c4506185e6fc7a5f27f88810d8b
hiroset0391162621 <takashi.hirose.b6@tohoku.ac.jp>
2024-09-18 00:09:33 +0900

    Update polanalysis_example_120s.py

--- example/polanalysis_example_120s.py
+++ example/polanalysis_example_120s.py
@@ -1,29 +1,17 @@
-# import copy
-# import cmath
 import sys
 import datetime
-# import pandas as pd
 import numpy as np
-# import scipy.signal as ssig
-#from scipy import *
-# from obspy import read
 import obspy
 from obspy.core import UTCDateTime
-# from obspy.signal.regression import linear_regression
 import matplotlib.pyplot as plt
 from matplotlib.ticker import *
 import warnings
 warnings.simplefilter('ignore')
-# from obspy.signal.trigger import classic_sta_lta
-# from obspy.signal.tf_misfit import cwt
 sys.path.append(sys.path.append("core/"))
 import backazimuth
 sys.path.append(sys.path.append("utils/"))
 import circular
-sys.path.append(sys.path.append("nipfilter/"))
-# from core import stransform, istransform
-# import filter as filt_par
-sys.path.append(sys.path.append("s-transform/"))
+import bootstrap    
 
 try:
     import scienceplots
@@ -88,63 +76,6 @@ gscmap = ListedColormap(gscolors)
 hsv2 = LinearSegmentedColormap.from_list("gscmap2", colors=gscolors)
 
 
-from scipy.stats import circmean, vonmises
-from scipy.special import i0
-
-def bootstrap_vm_confidence_interval(data, alpha=0.05, reps=1000):
-    """
-    Calculate the bootstrap confidence interval for the von Mises mean direction and concentration parameter.
-
-    :param data: array-like, circular data (in radians)
-    :param alpha: float, confidence level (default 0.05 for 95% confidence interval)
-    :param reps: int, number of bootstrap samples (default 1000)
-    :return: tuple, (mean_direction_low, mean_direction_high, kappa_low, kappa_high)
-    """
-    n = len(data)
-    bootstrap_means = []
-    bootstrap_kappas = []
-
-    def estimate_kappa(R, n):
-        """Estimate the concentration parameter kappa using the average resultant length R."""
-        if R < 0.53:
-            kappa = 2 * R + R**3 + (5 * R**5) / 6
-        elif R < 0.85:
-            kappa = -0.4 + 1.39 * R + 0.43 / (1 - R)
-        else:
-            kappa = 1 / (2 * (1 - R))
-        return kappa
-
-    for _ in range(reps):
-        # Resample with replacement
-        sample = np.random.choice(data, size=n, replace=True)
-        
-        # Compute the mean direction (in radians)
-        mean_direction = circmean(sample, high=np.pi, low=-np.pi)
-        bootstrap_means.append(mean_direction)
-        
-        # Compute resultant length R
-        C = np.sum(np.cos(sample)) / n
-        S = np.sum(np.sin(sample)) / n
-        R = np.sqrt(C**2 + S**2)
-        
-        # Estimate kappa (concentration parameter)
-        kappa = estimate_kappa(R, n)
-        bootstrap_kappas.append(kappa)
-
-    # Convert results to numpy arrays for easier manipulation
-    bootstrap_means = np.array(bootstrap_means)
-    bootstrap_kappas = np.array(bootstrap_kappas)
-    
-    # Compute percentiles for confidence intervals
-    mean_direction_low = np.percentile(bootstrap_means, (alpha / 2) * 100)
-    mean_direction_high = np.percentile(bootstrap_means, (1 - alpha / 2) * 100)
-    
-    kappa_low = np.percentile(bootstrap_kappas, (alpha / 2) * 100)
-    kappa_high = np.percentile(bootstrap_kappas, (1 - alpha / 2) * 100)
-
-    return mean_direction_low, mean_direction_high, kappa_low, kappa_high
-
-
 def ci_eachf(Fv, baz, Tv):
     time_baz_idx = np.where( (Tv[0,:]>=0) & (Tv[0,:]<=120) )[0]
     mean_direction_ci = np.zeros((Fv.shape[0], 3))*np.nan
@@ -155,7 +86,7 @@ def ci_eachf(Fv, baz, Tv):
         
         
         if Fv[i,0]>=0.0:
-            mean_direction_low, mean_direction_high, kappa_low, kappa_high = bootstrap_vm_confidence_interval(np.deg2rad(baz2))   
+            mean_direction_low, mean_direction_high, kappa_low, kappa_high = bootstrap.bootstrap_vm_confidence_interval(np.deg2rad(baz2))   
             mean_direction_low = np.rad2deg(mean_direction_low)
             mean_direction_high = np.rad2deg(mean_direction_high)
             print(str(Fv[i,0])+"Hz", mean_direction_low, mean_direction_high)
